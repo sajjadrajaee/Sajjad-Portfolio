6a36a1f773e211431169c6ed47968ba83ead3afd
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-05-13 01:52:48 +0430

    Fix footer height and add basic erro message

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+// Form Validation
+const x;
+x = document.forms['.contactForm']['.email'].value;
+const pattern = /[a-z]/.test(x);
+if (pattern != true) {
+  
+}
